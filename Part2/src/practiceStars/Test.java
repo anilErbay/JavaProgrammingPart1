a00bbb951228d5718d5239d7d623c2cf64bade83
anilErbay <anilernay@gmail.com>
2023-02-19 18:04:35 +0300

    check

---
+++ Part2/src/practiceStars/Test.java
@@ -0,0 +1,17 @@
+package practiceStars;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // input types
+        int number = Integer.parseInt(input.nextLine());
+        int number2 = input.nextInt();
+
+        double number3 = input.nextDouble();
+        boolean isValid = input.nextBoolean();
+
+    }
+}
